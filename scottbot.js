4b2f3f5df2e0949082398297a0d1cd3d101ac171
Aaron Bieber <qbit@deftly.net>
2011-05-12 13:16:40 -0700

    added CLI and accesslist stuff

--- scottbot.js
+++ scottbot.js
@@ -1,4 +1,8 @@
 require.paths.unshift("./node_modules");
+
+var sys = require("sys");
+var st = process.openStdin();
+
 var brain = require("brain"),
     irc = require("irc"),
     options = require("nomnom").opts({
@@ -17,6 +21,11 @@ var brain = require("brain"),
             default: "",
             help: "IRC channels to join (comma-separated, no '#')."
         },
+		allowed: {
+			string: "-a NICKS, --allowed=NICKS",
+			default: "",
+			help: "IRC nicks to allowed to teach ( comma-seperated )."
+		},
         redisHost: {
             string: "--redis-host=HOST",
             default: "localhost",
@@ -47,6 +56,8 @@ var bayes = new brain.BayesianClassifier({
 });
 
 var CHANNELS = options.channels.split(',');
+var ALLOWED = options.allowed.split(',');
+
 CHANNELS.forEach(function(channel, i) {
     CHANNELS[i] = '#' + channel.trim();
 });
@@ -55,9 +66,49 @@ var client = new irc.Client(options.host, options.nick, {
     channels: CHANNELS
 });
 
+// Bit to allow me to manually send input and add users to the include list
 client.addListener("error", function(msg) {
     console.log(msg);
 });
+
+st.addListener( "data", function(d) {
+    var string = d.toString().replace( /\n/, '' );
+    var parts = string.split( /:/ );
+
+    if ( parts[0] === "addUser" ) {
+        sys.print( "Adding user: " + parts[1] + "\n" );
+        ALLOWED.push( parts[1] );
+    }
+
+    if ( parts[0] === "addFunny" ) {
+        sys.print( "Adding funny phrase: " + parts[1] + "\n" );
+		bayes.train( parts[1], "funny", function() {
+			sys.print( "ok!"  + "\n");
+		});
+    }
+
+    if ( parts[0] === "rmFunny" ) {
+        sys.print( "Removing funny phrase: " + parts[1] + "\n" );
+		bayes.train( parts[1], "notfunny", function() {
+			sys.print( "sorry :("  + "\n");
+		});
+    }
+
+	sys.print(">");
+}).addListener( "end", function() {
+});
+
+sys.print(">");
+
+function oc( a ) {
+	var o = {};
+
+	for ( var i = 0; i < a.length; i++ ) {
+		o[ a[i] ] = "";
+	}
+
+	return o;
+}
 
 client.addListener("message", function(from, to, message) {
     var target, isChannel = false;
@@ -68,30 +119,47 @@ client.addListener("message", function(from, to, message) {
         target = from;
     }
 
-    if (isChannel) {
-        if (message.indexOf(options.nick) == 0) {
-            if (message.match(/no/i)) {
-                bayes.train(lastLine[target], "notfunny", function() {
-                    client.say(target, "sorry :(");
-                });
-            } else if (message.match(/yes/i)) {
-                bayes.train(lastLine[target], "funny", function() {
-                    client.say(target, "ok!");
-                });
-            } else if (message.match(/lol/i)) {
-                bayes.train(lastLine[target], "funny", function() {});
-            } else if (message.match(/botsnack/i)) {
-                client.say(target, "nom nom nom");
-            }
-        } else {
-            lastLine[target] = message;
-            bayes.classify(message, function(category) {
-                if (category == "funny") {
-                    client.say(target, "that's what she said");
-                }
-            });
-        }
-    }
+	if (isChannel) {
+		if (message.indexOf(options.nick) == 0) {
+			if (message.match(/no/i)) {
+				if ( from in oc( ALLOWED )) {
+					bayes.train(lastLine[target], "notfunny", function() {
+						client.say(target, "sorry :(");
+					});
+				} else {
+					sys.print( "blocking learn request from: " + from + "\n>" );
+				}
+			} else if (message.match(/yes/i)) {
+				if ( from in oc( ALLOWED )) {
+					bayes.train(lastLine[target], "funny", function() {
+						client.say(target, "ok!");
+					});
+				} else {
+					sys.print( "blocking learn request from: " + from + "\n>" );
+				}
+			} else if (message.match(/lol/i)) {
+				bayes.train(lastLine[target], "funny", function() {});
+			} else if (message.match(/br/i)) {
+				client.say(target, "Bootie Rockin!");
+			} else if (message.match(/hack/i)) {
+				client.say(target, "I phear you have rooted me!!");
+			} else if (message.match(/o\//i)) {
+				client.say(target, "\\o");
+			} else if (message.match(/lsAllowed/i)) {
+				client.say(target, ALLOWED.join( ", " ) );
+			} else if (message.match(/botsnack/i)) {
+				client.say(target, "nom nom nom");
+			}
+		} else {
+			lastLine[target] = message;
+			bayes.classify(message, function(category) {
+				if (category == "funny") {
+					client.say(target, "that's what she said");
+				}
+			});
+		}
+	}
+
 });
 
 client.addListener("invite", function(channel, from) {
